23912c4d632a7e5ec58d2d5877d7f7cac2555afb
just-kk2001 <kaustubhkumbhar2468@gmail.com>
2024-09-06 06:25:17 +0000

    container chnages done

---
+++ javaS/Var.js
@@ -0,0 +1,13 @@
+const accountid =101122 //unchangable
+let mail='kaustubhkumbhar24682gmail.com'
+var pswd =2364
+accountCity= "Jaypur"
+console.log([accountid,mail,pswd,accountCity])
+mail='Shraddha@gmail.com'
+pswd='2222'
+accountcity='pune'
+console.log([mail,pswd,accountCity])
+
+/*
+Dont use VAr
+*/
