86a37d35d3c30f1a94deecd0b3b1bbc67e468e76
just-kk2001 <kaustubhkumbhar2468@gmail.com>
2024-09-06 06:18:28 +0000

    done

--- javaS/var.js
+++ javaS/var.js
@@ -1,3 +1,5 @@
 const accountid =101122 //unchangable
-let mail="kaustubhkumbhar24682gmail.com"
-var pswd ="2364"
+let mail='kaustubhkumbhar24682gmail.com'
+var pswd =2364
+
+console.log([accountid,mail,pswd,accountCity])
