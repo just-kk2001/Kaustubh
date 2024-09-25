64db9e69b891b505ff2054423acac8c2f22b9d4a
just-kk2001 <kaustubhkumbhar2468@gmail.com>
2024-09-25 15:32:33 +0530

    Add files via upload

---
+++ Task5.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+fetch('https://jsonplaceholder.typicode.com/users/').then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+}).catch((error)=>console.log(error))
---
+++ task4.js
@@ -0,0 +1,18 @@
+async function  getAllUsers() {
+        try {
+        
+         const response=await fetch('https://jsonplaceholder.typicode.com/users/')
+         console.log(response)
+         const data= await response.json()
+         console.log(data)
+         
+        } catch (error) {
+         console.log("E:",error);
+        
+        
+        }
+     }
+     getAllUsers() 
+     setTimeout(function(e){
+         getAllUsers() 
+     },10000) 
